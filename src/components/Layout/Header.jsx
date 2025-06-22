2490eb28022a6e4d22caedd6d2c105115e56847e
Simon <omojide.o.j@gmail.com>
2025-06-22 21:54:43 +0100

    Add Header.jsx and Header.module.css

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const Header = () => {
+ return <Fragment>
+  <header>
+    <h1>Healthy Meal</h1>
+  </header>
+  <div></div>
+ </Fragment>
+}
+
+export default Header;
