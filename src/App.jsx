c1aea86325e0fe1fb529c0488fee910efafa1c47
Simon <omojide.o.j@gmail.com>
2025-06-23 21:59:51 +0100

    Add Card.jsx and Mealitem sub-folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Header from "./components/Layout/Header";
+import { Fragment } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<Fragment>
+			<Header />
+			<main>
+				<Meals />
+			</main>
+		</Fragment>
+	);
 }
 
 export default App;
