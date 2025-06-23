74515382fcd97ddbc76423273fdc0693cbd5140c
Simon <omojide.o.j@gmail.com>
2025-06-23 23:52:03 +0100

    Implement reuseable input.jsx file, create Cart.jsx

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,15 @@
+import classes from './Input.module.css';
+
+const Input = (props) => {
+	return (
+		<div className={classes.input}>
+			<label htmlFor={props.input.id}>{props.label}</label>
+			<input
+				id={props.input.id}
+				{...props.input} // Spread operator to pass all other props like type, value, onChange, etc.
+			/>
+		</div>
+	);
+};
+
+export default Input;
