638429a33d3251100ecb3e0579663977d9aa744d
Simon <omojide.o.j@gmail.com>
2025-06-23 12:41:08 +0100

    Add seperate CartButton.jsx and ccs module styles

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,12 +1,24 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
+import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpg';
+
+const image = {
+  content: mealsImage,
+  name: 'A table full of delicious food'
+}
 
 const Header = () => {
- return <Fragment>
-  <header>
-    <h1>Healthy Meal</h1>
-  </header>
-  <div></div>
- </Fragment>
-}
+	return (
+		<Fragment>
+			<header className={classes.header}>
+				<h1>Healthy Meal</h1>
+        <button>Cart</button>
+			</header>
+			<div className={classes['main-image']}>
+        <img src={image.content} alt={image.name} />
+      </div>
+		</Fragment>
+	);
+};
 
 export default Header;
