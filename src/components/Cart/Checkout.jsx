fcb9097ac6e1aa2ac9984892731a55ba043e58fb
Simon <omojide.o.j@gmail.com>
2025-07-08 14:12:45 +0100

    Add checkout form

---
+++ src/components/Cart/Checkout.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import classes from './Checkout.module.css';
+
+export const Checkout = () => {
+  return (
+    <form action="">
+      <div className={classes.control}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id='name' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id='street' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="busstop">Bus-Stop</label>
+        <input type="text" id='busstop' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" id='city' />
+      </div>
+      <button>Confirm</button>
+    </form>
+  )
+}
+
+export default Checkout;
