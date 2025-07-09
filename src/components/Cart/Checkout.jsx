5f75f22e3d9ee86d997e2a6a5c3ea6fd448265ca
Simon <omojide.o.j@gmail.com>
2025-07-09 17:04:07 +0100

    completed form checkout validation

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -14,32 +14,81 @@ const Checkout = (props) => {
 		hasError: nameHasError,
 	} = useCheckout(validateInput);
 
+	const {
+		value: enteredStreetName,
+		isValid: streetIsValid,
+		inputChangeHandler: streetChangeHandler,
+		inputBlurHandler: streetBlur,
+		resetHandler: resetStreet,
+		hasError: streetNameHasError,
+	} = useCheckout(validateInput);
+
+	const {
+		value: enteredBusStopName,
+		isValid: busStopIsValid,
+		inputChangeHandler: busStopChangeHandler,
+		inputBlurHandler: busStopBlur,
+		resetHandler: resetBusStop,
+		hasError: busStopNameHasError,
+	} = useCheckout(validateInput);
+
+	const {
+		value: enteredCityName,
+		isValid: cityIsValid,
+		inputChangeHandler: cityChangeHandler,
+		inputBlurHandler: cityBlur,
+		resetHandler: resetCity,
+		hasError: cityNameHasError,
+	} = useCheckout(validateInput);
+
 	let formIsValid = false;
-	if (nameIsValid) {
+	if (nameIsValid && streetIsValid && busStopIsValid && cityIsValid) {
 		formIsValid = true;
 	}
 
 	const formConfirmHandler = (e) => {
 		e.preventDefault();
-		if (nameHasError) {
+		if (!formIsValid) {
+			nameBlur();
+			streetBlur();
+			busStopBlur();
+			cityBlur();
 			return;
 		}
 
 		const formData = {
 			name: enteredName,
+			street: enteredStreetName,
+			busStop: enteredBusStopName,
+			city: enteredCityName,
 		};
 
 		console.log('This is fornData', formData);
 
 		resetName();
+		resetStreet();
+		resetBusStop();
+		resetCity();
 	};
 
 	let nameClassList = nameHasError
 		? `${classes.control} ${classes.invalid}`
 		: `${classes.control}`;
 
+	let streetNameClassList = streetNameHasError
+		? `${classes.control} ${classes.invalid}`
+		: `${classes.control}`;
+
+	let busStopNameClassList = busStopNameHasError
+		? `${classes.control} ${classes.invalid}`
+		: `${classes.control}`;
+
+	let cityNameClassList = cityNameHasError
+		? `${classes.control} ${classes.invalid}`
+		: `${classes.control}`;
+
 	return (
-		<form onSubmit={formConfirmHandler}>
+		<form className={classes.form} onSubmit={formConfirmHandler}>
 			<div className={nameClassList}>
 				<label htmlFor="name">Your Name</label>
 				<input
@@ -49,23 +98,47 @@ const Checkout = (props) => {
 					onChange={nameChangeHandler}
 					onBlur={nameBlur}
 				/>
+				{nameHasError && <p className={classes.error}>Plaese enter a Valid name!</p>}
 			</div>
-			<div className={classes.control}>
+			<div className={streetNameClassList}>
 				<label htmlFor="street">Street</label>
-				<input type="text" id="street" />
+				<input
+					type="text"
+					id="street"
+					value={enteredStreetName}
+					onChange={streetChangeHandler}
+					onBlur={streetBlur}
+				/>
+				{streetNameHasError && <p className={classes.error}>Please enter a valid street name!</p>}
 			</div>
-			<div className={classes.control}>
+			<div className={busStopNameClassList}>
 				<label htmlFor="busstop">Bus-Stop</label>
-				<input type="text" id="busstop" />
+				<input
+					type="text"
+					id="busstop"
+					value={enteredBusStopName}
+					onChange={busStopChangeHandler}
+					onBlur={busStopBlur}
+				/>
+				{busStopNameHasError && <p className={classes.error}>Please enter a valid bus-stop name!</p>}
 			</div>
-			<div className={classes.control}>
+			<div className={cityNameClassList}>
 				<label htmlFor="city">City</label>
-				<input type="text" id="city" />
+				<input
+					type="text"
+					id="city"
+					value={enteredCityName}
+					onChange={cityChangeHandler}
+					onBlur={cityBlur}
+				/>
+        {cityNameHasError && <p className={classes.error}>Please enter a valid city address!</p>}
 			</div>
-			<button type="button" onClick={props.onCancel}>
-				Cancel
-			</button>
-			<button disabled={!formIsValid}>Confirm</button>
+			<div className={classes.actions}>
+				<button type="button" onClick={props.onCancel}>
+					Cancel
+				</button>
+				<button className={classes.submit}>Confirm</button>
+			</div>
 		</form>
 	);
 };
